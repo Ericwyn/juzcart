4ebd49770e0f491c9fe0973c714bdb5b9aea581e
Ericwyn <Ericwyn.chen@gmail.com>
2018-11-27 19:03:57 +0800

    refactor: move the project Juzcar the juzcar is move to Https://github.com/Ericwyn/Juzcar

--- src/main/java/com/ericwyn/juzcart/TestMain.java
+++ src/main/java/com/ericwyn/juzcart/TestMain.java
@@ -1,13 +1,23 @@
 package com.ericwyn.juzcart;
 
 import com.ericwyn.juzcar.JuzcarServer;
+import com.ericwyn.juzcar.scan.cb.JuzcarScannerCb;
+import com.ericwyn.juzcar.scan.obj.JuzcarApi;
+
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by Ericwyn on 18-11-26.
  */
 public class TestMain {
     public static void main(String[] args) {
-        JuzcarServer.run(JuzcartApplication.class);
+        JuzcarServer.run(JuzcartApplication.class, new JuzcarScannerCb() {
+            @Override
+            public void callback(HashMap<String, List<JuzcarApi>> apis) {
+                System.out.println(apis.size());
+            }
+        });
     }
 
 }
